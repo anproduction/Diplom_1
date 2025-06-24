c6a9d7253b7ac07a89fcf799a7a94a978fc79295
Anastasia Prodan <prodananastasija@yandex.com>
2025-06-24 20:12:09 +0300

    fix_test_database

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,34 +1,28 @@
-import pytest
 from praktikum.database import Database
 from praktikum.ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING
+from data import EXPECTED_BUNS, EXPECTED_SAUCES, EXPECTED_FILLINGS
 
 
-@pytest.fixture
-def db():
-    return Database()
+class TestDatabase:
 
+    def test_available_buns(self):
+        db = Database()
+        buns = db.available_buns()
+        assert len(buns) == len(EXPECTED_BUNS)
 
-def test_available_buns(db):
-    buns = db.available_buns()
-    assert len(buns) == 3
+        actual = [(bun.get_name(), bun.get_price()) for bun in buns]
+        assert actual == EXPECTED_BUNS
 
-    expected = [("black bun", 100), ("white bun", 200), ("red bun", 300)]
-    actual = [(bun.get_name(), bun.get_price()) for bun in buns]
-    assert actual == expected
+    def test_available_ingredients(self):
+        db = Database()
+        ingredients = db.available_ingredients()
+        assert len(ingredients) == len(EXPECTED_SAUCES) + len(EXPECTED_FILLINGS)
 
+        sauces = ingredients[:len(EXPECTED_SAUCES)]
+        fillings = ingredients[len(EXPECTED_SAUCES):]
 
-def test_available_ingredients(db):
-    ingredients = db.available_ingredients()
-    assert len(ingredients) == 6
+        actual_sauces = [(i.get_name(), i.get_price()) for i in sauces if i.get_type() == INGREDIENT_TYPE_SAUCE]
+        actual_fillings = [(i.get_name(), i.get_price()) for i in fillings if i.get_type() == INGREDIENT_TYPE_FILLING]
 
-    sauces = ingredients[:3]
-    fillings = ingredients[3:]
-
-    expected_sauces = [("hot sauce", 100), ("sour cream", 200), ("chili sauce", 300)]
-    expected_fillings = [("cutlet", 100), ("dinosaur", 200), ("sausage", 300)]
-
-    actual_sauces = [(i.get_name(), i.get_price()) for i in sauces if i.get_type() == INGREDIENT_TYPE_SAUCE]
-    actual_fillings = [(i.get_name(), i.get_price()) for i in fillings if i.get_type() == INGREDIENT_TYPE_FILLING]
-
-    assert actual_sauces == expected_sauces
-    assert actual_fillings == expected_fillings
+        assert actual_sauces == EXPECTED_SAUCES
+        assert actual_fillings == EXPECTED_FILLINGS
