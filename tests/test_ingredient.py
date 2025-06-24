effeb985c2bac984bca79156318494e462b114ad
Anastasia Prodan <prodananastasija@yandex.com>
2025-06-24 20:16:37 +0300

    fix_test_ingredient

--- tests/test_ingredient.py
+++ tests/test_ingredient.py
@@ -1,20 +1,27 @@
 import pytest
 from praktikum.ingredient import Ingredient
-from praktikum.ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING
 from praktikum import ingredient_types
+from data import INGREDIENT_TEST_DATA
 
-@pytest.mark.parametrize("ingredient_type, name, price", [
-    (INGREDIENT_TYPE_SAUCE, "Барбекю", 50.0),
-    (INGREDIENT_TYPE_FILLING, "Котлета", 150.0),
-    ("UNKNOWN", "Что-то странное", 0.0),
-])
-def test_ingredient_initialization_and_getters(ingredient_type, name, price):
+
+@pytest.mark.parametrize("ingredient_type, name, price", INGREDIENT_TEST_DATA)
+def test_ingredient_get_type(ingredient_type, name, price):
     ingredient = Ingredient(ingredient_type, name, price)
-
     assert ingredient.get_type() == ingredient_type
+
+
+@pytest.mark.parametrize("ingredient_type, name, price", INGREDIENT_TEST_DATA)
+def test_ingredient_get_name(ingredient_type, name, price):
+    ingredient = Ingredient(ingredient_type, name, price)
     assert ingredient.get_name() == name
+
+
+@pytest.mark.parametrize("ingredient_type, name, price", INGREDIENT_TEST_DATA)
+def test_ingredient_get_price(ingredient_type, name, price):
+    ingredient = Ingredient(ingredient_type, name, price)
     assert ingredient.get_price() == price
 
+
 def test_ingredient_types_constants():
-    assert ingredient_types.INGREDIENT_TYPE_SAUCE == 'SAUCE'
-    assert ingredient_types.INGREDIENT_TYPE_FILLING == 'FILLING'
+    assert ingredient_types.INGREDIENT_TYPE_SAUCE == "SAUCE"
+    assert ingredient_types.INGREDIENT_TYPE_FILLING == "FILLING"
